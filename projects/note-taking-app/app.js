d786fbbb34861bb97e767db7199108e6fe30ffb5
Aquila Vasconcelos <aquilavasconcelos@hotmail.com>
2021-11-28 15:45:48 -0300

    Adding package json, git gnore and trying out new dependencies as validator and chalk.

--- projects/note-taking-app/app.js
+++ projects/note-taking-app/app.js
@@ -1,11 +1,15 @@
 // const add = require('./utils');
-
 // const sum = add(5, 3);
-
 // console.log(sum)
 
+const validator = require('validator');
+// console.log(validator.isEmail('aquilavasconcelos@hotmail.com'));
+// console.log(validator.isURL('aquila.vasconcelos.com'));
+
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const yourNotes = getNotes();
-
 console.log(yourNotes);
+
+console.log(chalk.green.bold.bgMagenta.inverse('Success!'));
