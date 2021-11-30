91e28c06e5665645a34782ee95638f81b4b64ebf
Aquila Vasconcelos <aquilavasconcelos@hotmail.com>
2021-11-29 21:42:34 -0300

    Installing yargs in our project + Setting yargs version + Setting commands for our crud functions for notes with help of yargs.

--- projects/note-taking-app/app.js
+++ projects/note-taking-app/app.js
@@ -1,15 +1,44 @@
-// const add = require('./utils');
-// const sum = add(5, 3);
-// console.log(sum)
-
-const validator = require('validator');
-// console.log(validator.isEmail('aquilavasconcelos@hotmail.com'));
-// console.log(validator.isURL('aquila.vasconcelos.com'));
-
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const yourNotes = getNotes();
-console.log(yourNotes);
+// Customize yargs
+yargs.version('17.2.1');
 
-console.log(chalk.green.bold.bgMagenta.inverse('Success!'));
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding new note');
+    }
+});
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing the note');
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler: () => {
+        console.log('Reading the note')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Presents the list of notes',
+    handler: () => {
+        console.log(getNotes());
+    }
+})
+
+console.log(yargs.argv);
