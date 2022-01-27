9bd0f6fcbb48e118addcc66fa477e40b818415d6
Aquila Vasconcelos <aquilavasconcelos@hotmail.com>
2022-01-27 09:06:46 -0300

    Removing static files + Adding dependency to hbs (hendlebars) + Creating views using .hbs files + Creating routes to use the new views created

--- projects/web-server/src/app.js
+++ projects/web-server/src/app.js
@@ -4,7 +4,31 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, './../public');
 
+// needs to be exactly like below 'view engine'
+app.set('view engine', 'hbs');
+
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Aquila Vasconcelos'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Aquila Vasconcelos'
+  })
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Looking for Help?',
+    message: 'This is my help page'
+  })
+});
 
 app.get('/weather', (req, res) => {
   res.send({
