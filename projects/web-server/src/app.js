ea5c892277e9dedf139a4434e0c0278354d9b193
Aquila Vasconcelos <aquilavasconcelos@hotmail.com>
2022-01-28 21:14:33 -0300

    Changing location of views from views to templates/views + Customizing location of views to express as the pattern one is views + Creating a new partial and using the partial in the views.

--- projects/web-server/src/app.js
+++ projects/web-server/src/app.js
@@ -1,12 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, './../public');
+const viewsPath = path.join(__dirname, './../templates/views');
+const partialsPath = path.join(__dirname, './../templates/partials');
 
-// needs to be exactly like below 'view engine'
+// Setup handlebars engine and views location, respectively
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
